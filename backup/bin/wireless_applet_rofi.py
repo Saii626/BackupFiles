3f79096337efdf4205a430a47ad4111472a004cb
Saii <saikat626@gmail.com>
2020-12-31 00:00:00 +0530

    Autocommit at 2020-12-31T00:00:00

--- backup/bin/wireless_applet_rofi.py
+++ backup/bin/wireless_applet_rofi.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
-import os, re
+import os, re, sys, time
 from subprocess import Popen, run, PIPE
 from utils import run_rofi, notify
 
-iwctl = run(['iwctl'], stdin=PIPE, capture_output=True, check=True)
+iwctl = Popen(['iwctl'], stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding='utf-8')
+
+iwctl.stdin.write('station wlan0 show')
+time.sleep(1)
+print('Err: "%s"'%iwctl.stderr.read(2))
+print('Out: "%s"'%iwctl.stdout.read(100))
+
+
+cmd = input("Cmd:")
+outs, errs = iwctl.stdin.write(cmd)
+print('Err: "%s"'%errs.decode('utf-8'))
+print('Out: "%s"'%outs.decode('utf-8'))
